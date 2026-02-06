b8e3debe9303812576cec95ecab9edd980bef748
shajimahe <alabrargit25@gmail.com>
2026-02-06 13:40:56 +0530

    base repo changes

--- Repository/BaseRepostitory.cs
+++ Repository/BaseRepostitory.cs
@@ -29,36 +29,21 @@ namespace Repository
                         using (StreamReader sr = new StreamReader(txtpath))
                         {
                             string ss = sr.ReadLine();
-                            if (string.IsNullOrWhiteSpace(ss))
-                                throw new Exception("Config file is empty");
-
                             string[] txtsplit = ss.Split(';');
-                            if (txtsplit.Length < 4)
-                                throw new Exception("Config file format is invalid. Expected: Server;Database;UserID;Password");
+                            string server = txtsplit[0].ToString();
+                            string DataBase = txtsplit[1].ToString();
+                            string userid = txtsplit[2].ToString(); // user id
+                            string password = txtsplit[3].ToString(); // password
+                                                                      // string Security = txtsplit[3].ToString();
+                            string Local = server + ';' + DataBase + ';' + userid + ';' + password + ';';//+ Security
 
-                            string server = txtsplit[0].Trim();
-                            string database = txtsplit[1].Trim();
-                            string userid = txtsplit[2].Trim(); // user id
-                            string password = txtsplit[3].Trim(); // password
-
-                            // Build proper SQL connection string
-                            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                            builder.DataSource = server;
-                            builder.InitialCatalog = database;
-                            builder.UserID = userid;
-                            builder.Password = password;
-                            builder.TrustServerCertificate = true;
-
-                            DataConnection = new SqlConnection(builder.ConnectionString);
+                            DataConnection = new SqlConnection(Local);
                         }
-                    }
-                    else
-                    {
-                        throw new FileNotFoundException($"Database configuration file not found at: {txtpath}. Please create the config file with format: Server;Database;UserID;Password");
                     }
                 }
                 catch (Exception ex)
                 {
+
                     throw;
                 }
             }
