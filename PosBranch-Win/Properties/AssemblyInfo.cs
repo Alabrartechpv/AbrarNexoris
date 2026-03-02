860b28d437ae44782d26482f36cbcb1eac827e98
Vrinda Vinod <vrinda.alabrar@outlook.com>
2026-03-02 10:28:11 +0530

    j

--- PosBranch-Win/Properties/AssemblyInfo.cs
+++ PosBranch-Win/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PosBranch-Win")]
+[assembly: AssemblyTitle("NexorisPOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PosBranch-Win")]
+[assembly: AssemblyProduct("NexorisPOS")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
